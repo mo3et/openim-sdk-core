4914d37349db5002dc99ba3c54759b330f46d3c8
withchao <993506633@qq.com>
2024-11-15 10:37:58 +0800

    format

--- bindings/base/api_registry.go
+++ bindings/base/api_registry.go
@@ -10,8 +10,6 @@ import (
 	pb "github.com/openimsdk/openim-sdk-core/v3/proto"
 )
 
-var FuncMap = make(map[pb.FuncRequestEventName]callFunc)
-
 func Func[A, B any](fn func(ctx context.Context, req *A) (*B, error)) callFunc {
 	return func(ctx context.Context, req []byte) ([]byte, error) {
 		var pbReq A
@@ -39,35 +37,33 @@ func Func[A, B any](fn func(ctx context.Context, req *A) (*B, error)) callFunc {
 	}
 }
 
-func init() {
-
-	FuncMap[pb.FuncRequestEventName_InitSDK] = Func(open_im_sdk.UserForSDK.InitSDK)
-	FuncMap[pb.FuncRequestEventName_Login] = Func(open_im_sdk.UserForSDK.Login)
-	FuncMap[pb.FuncRequestEventName_GetAllConversationList] = Func(open_im_sdk.UserForSDK.Conversation().GetAllConversationList)
-
-	FuncMap[pb.FuncRequestEventName_CreateGroup] = Func(open_im_sdk.UserForSDK.Group().CreateGroup)
-	FuncMap[pb.FuncRequestEventName_JoinGroup] = Func(open_im_sdk.UserForSDK.Group().JoinGroup)
-	FuncMap[pb.FuncRequestEventName_QuitGroup] = Func(open_im_sdk.UserForSDK.Group().QuitGroup)
-	FuncMap[pb.FuncRequestEventName_DismissGroup] = Func(open_im_sdk.UserForSDK.Group().DismissGroup)
-	FuncMap[pb.FuncRequestEventName_ChangeGroupMute] = Func(open_im_sdk.UserForSDK.Group().ChangeGroupMute)
-	FuncMap[pb.FuncRequestEventName_ChangeGroupMemberMute] = Func(open_im_sdk.UserForSDK.Group().ChangeGroupMemberMute)
-	FuncMap[pb.FuncRequestEventName_TransferGroupOwner] = Func(open_im_sdk.UserForSDK.Group().TransferGroupOwner)
-	FuncMap[pb.FuncRequestEventName_KickGroupMember] = Func(open_im_sdk.UserForSDK.Group().KickGroupMember)
-	FuncMap[pb.FuncRequestEventName_SetGroupInfo] = Func(open_im_sdk.UserForSDK.Group().SetGroupInfo)
-	FuncMap[pb.FuncRequestEventName_SetGroupMemberInfo] = Func(open_im_sdk.UserForSDK.Group().SetGroupMemberInfo)
-	FuncMap[pb.FuncRequestEventName_GetJoinedGroups] = Func(open_im_sdk.UserForSDK.Group().GetJoinedGroups)
-	//FuncMap[pb.FuncRequestEventName_GetJoinedGroupListPage] = Func(open_im_sdk.UserForSDK.Group().GetJoinedGroupListPage)
-	//FuncMap[pb.FuncRequestEventName_GetSpecifiedGroupsInfo] = Func(open_im_sdk.UserForSDK.Group().GetSpecifiedGroupsInfo)
-	//FuncMap[pb.FuncRequestEventName_SearchGroups] = Func(open_im_sdk.UserForSDK.Group().SearchGroups)
-	//FuncMap[pb.FuncRequestEventName_GetGroupMemberOwnerAndAdmin] = Func(open_im_sdk.UserForSDK.Group().GetGroupMemberOwnerAndAdmin)
-	//FuncMap[pb.FuncRequestEventName_GetGroupMemberListByJoinTimeFilter] = Func(open_im_sdk.UserForSDK.Group().GetGroupMemberListByJoinTimeFilter)
-	//FuncMap[pb.FuncRequestEventName_GetSpecifiedGroupMembersInfo] = Func(open_im_sdk.UserForSDK.Group().GetSpecifiedGroupMembersInfo)
-	//FuncMap[pb.FuncRequestEventName_GetGroupMemberList] = Func(open_im_sdk.UserForSDK.Group().GetGroupMemberList)
-	//FuncMap[pb.FuncRequestEventName_GetGroupRequest] = Func(open_im_sdk.UserForSDK.Group().GetGroupRequest)
-	//FuncMap[pb.FuncRequestEventName_SearchGroupMembers] = Func(open_im_sdk.UserForSDK.Group().SearchGroupMembers)
-	FuncMap[pb.FuncRequestEventName_IsJoinGroup] = Func(open_im_sdk.UserForSDK.Group().IsJoinGroup)
-	FuncMap[pb.FuncRequestEventName_GetUsersInGroup] = Func(open_im_sdk.UserForSDK.Group().GetUsersInGroup)
-	FuncMap[pb.FuncRequestEventName_InviteUserToGroup] = Func(open_im_sdk.UserForSDK.Group().InviteUserToGroup)
-	FuncMap[pb.FuncRequestEventName_HandlerGroupRequest] = Func(open_im_sdk.UserForSDK.Group().HandlerGroupRequest)
-	FuncMap[pb.FuncRequestEventName_GetGroupMemberInfo] = Func(open_im_sdk.UserForSDK.Group().GetGroupMemberInfo)
+var FuncMap = map[pb.FuncRequestEventName]callFunc{
+	pb.FuncRequestEventName_InitSDK:                            Func(open_im_sdk.UserForSDK.InitSDK),
+	pb.FuncRequestEventName_Login:                              Func(open_im_sdk.UserForSDK.Login),
+	pb.FuncRequestEventName_GetAllConversationList:             Func(open_im_sdk.UserForSDK.Conversation().GetAllConversationList),
+	pb.FuncRequestEventName_CreateGroup:                        Func(open_im_sdk.UserForSDK.Group().CreateGroup),
+	pb.FuncRequestEventName_JoinGroup:                          Func(open_im_sdk.UserForSDK.Group().JoinGroup),
+	pb.FuncRequestEventName_QuitGroup:                          Func(open_im_sdk.UserForSDK.Group().QuitGroup),
+	pb.FuncRequestEventName_DismissGroup:                       Func(open_im_sdk.UserForSDK.Group().DismissGroup),
+	pb.FuncRequestEventName_ChangeGroupMute:                    Func(open_im_sdk.UserForSDK.Group().ChangeGroupMute),
+	pb.FuncRequestEventName_ChangeGroupMemberMute:              Func(open_im_sdk.UserForSDK.Group().ChangeGroupMemberMute),
+	pb.FuncRequestEventName_TransferGroupOwner:                 Func(open_im_sdk.UserForSDK.Group().TransferGroupOwner),
+	pb.FuncRequestEventName_KickGroupMember:                    Func(open_im_sdk.UserForSDK.Group().KickGroupMember),
+	pb.FuncRequestEventName_SetGroupInfo:                       Func(open_im_sdk.UserForSDK.Group().SetGroupInfo),
+	pb.FuncRequestEventName_SetGroupMemberInfo:                 Func(open_im_sdk.UserForSDK.Group().SetGroupMemberInfo),
+	pb.FuncRequestEventName_GetJoinedGroups:                    Func(open_im_sdk.UserForSDK.Group().GetJoinedGroups),
+	pb.FuncRequestEventName_GetJoinedGroupListPage:             Func(open_im_sdk.UserForSDK.Group().GetJoinedGroupListPage),
+	pb.FuncRequestEventName_GetSpecifiedGroupsInfo:             Func(open_im_sdk.UserForSDK.Group().GetSpecifiedGroupsInfo),
+	pb.FuncRequestEventName_SearchGroups:                       Func(open_im_sdk.UserForSDK.Group().SearchGroups),
+	pb.FuncRequestEventName_GetGroupMemberOwnerAndAdmin:        Func(open_im_sdk.UserForSDK.Group().GetGroupMemberOwnerAndAdmin),
+	pb.FuncRequestEventName_GetGroupMemberListByJoinTimeFilter: Func(open_im_sdk.UserForSDK.Group().GetGroupMemberListByJoinTimeFilter),
+	pb.FuncRequestEventName_GetSpecifiedGroupMembersInfo:       Func(open_im_sdk.UserForSDK.Group().GetSpecifiedGroupMembersInfo),
+	pb.FuncRequestEventName_GetGroupMemberList:                 Func(open_im_sdk.UserForSDK.Group().GetGroupMemberList),
+	pb.FuncRequestEventName_GetGroupRequest:                    Func(open_im_sdk.UserForSDK.Group().GetGroupRequest),
+	pb.FuncRequestEventName_SearchGroupMembers:                 Func(open_im_sdk.UserForSDK.Group().SearchGroupMembers),
+	pb.FuncRequestEventName_IsJoinGroup:                        Func(open_im_sdk.UserForSDK.Group().IsJoinGroup),
+	pb.FuncRequestEventName_GetUsersInGroup:                    Func(open_im_sdk.UserForSDK.Group().GetUsersInGroup),
+	pb.FuncRequestEventName_InviteUserToGroup:                  Func(open_im_sdk.UserForSDK.Group().InviteUserToGroup),
+	pb.FuncRequestEventName_HandlerGroupRequest:                Func(open_im_sdk.UserForSDK.Group().HandlerGroupRequest),
+	pb.FuncRequestEventName_GetGroupMemberInfo:                 Func(open_im_sdk.UserForSDK.Group().GetGroupMemberInfo),
 }
