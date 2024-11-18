92e5e5bf8460287b97225ff344a995b813221717
withchao <993506633@qq.com>
2024-11-18 17:24:35 +0800

    msg

--- internal/conversation_msg/conversion_1.go
+++ internal/conversation_msg/conversion_1.go
@@ -11,28 +11,29 @@ import (
 )
 
 /*
-  Text
-  Picture
-  Sound
-  Video
-  File
-  AtText
-  Merger
-  Card
-  Location
-  Custom
-  Typing
-  Quote
-  Face
-  AdvancedText
-  CustomMsgNotTriggerConversation
-  CustomMsgOnlineOnly
+  sdkpb.ContentType_Text
+  sdkpb.ContentType_Picture
+  sdkpb.ContentType_Typing
+  sdkpb.ContentType_Video
+  sdkpb.ContentType_File
+  sdkpb.ContentType_AtText
+  sdkpb.ContentType_Merger
+  sdkpb.ContentType_Card
+  sdkpb.ContentType_Location
+  sdkpb.ContentType_Custom
+  sdkpb.ContentType_Typing
+  sdkpb.ContentType_Quote
+  sdkpb.ContentType_Face
+  sdkpb.ContentType_AdvancedText
+  sdkpb.ContentType_CustomMsgNotTriggerConversation
+  sdkpb.ContentType_CustomMsgOnlineOnly
 */
 
 type mateType struct {
-	New func() any // elem
-	Get func(oneof any) any
-	Set func(msg *sdkpb.IMMessage, elem any)
+	New          func() any // elem
+	Get          func(oneof any) any
+	Set          func(msg *sdkpb.IMMessage, elem any)
+	Notification bool
 }
 
 var MateTypeMap = map[sdkpb.ContentType]mateType{
@@ -45,6 +46,149 @@ var MateTypeMap = map[sdkpb.ContentType]mateType{
 		},
 		Set: func(msg *sdkpb.IMMessage, elem any) {
 			msg.Content = &sdkpb.IMMessage_TextElem{TextElem: elem.(*sdkpb.TextElem)}
+		},
+	},
+	sdkpb.ContentType_Picture: {
+		New: func() any {
+			return &sdkpb.PictureElem{}
+		},
+		Get: func(oneof any) any {
+			return oneof.(*sdkpb.IMMessage_PictureElem).PictureElem
+		},
+		Set: func(msg *sdkpb.IMMessage, elem any) {
+			msg.Content = &sdkpb.IMMessage_PictureElem{PictureElem: elem.(*sdkpb.PictureElem)}
+		},
+	},
+	sdkpb.ContentType_Sound: {
+		New: func() any {
+			return &sdkpb.SoundElem{}
+		},
+		Get: func(oneof any) any {
+			return oneof.(*sdkpb.IMMessage_SoundElem).SoundElem
+		},
+		Set: func(msg *sdkpb.IMMessage, elem any) {
+			msg.Content = &sdkpb.IMMessage_SoundElem{SoundElem: elem.(*sdkpb.SoundElem)}
+		},
+	},
+	sdkpb.ContentType_Video: {
+		New: func() any {
+			return &sdkpb.VideoElem{}
+		},
+		Get: func(oneof any) any {
+			return oneof.(*sdkpb.IMMessage_VideoElem).VideoElem
+		},
+		Set: func(msg *sdkpb.IMMessage, elem any) {
+			msg.Content = &sdkpb.IMMessage_VideoElem{VideoElem: elem.(*sdkpb.VideoElem)}
+		},
+	},
+	sdkpb.ContentType_File: {
+		New: func() any {
+			return &sdkpb.FileElem{}
+		},
+		Get: func(oneof any) any {
+			return oneof.(*sdkpb.IMMessage_FileElem).FileElem
+		},
+		Set: func(msg *sdkpb.IMMessage, elem any) {
+			msg.Content = &sdkpb.IMMessage_FileElem{FileElem: elem.(*sdkpb.FileElem)}
+		},
+	},
+	sdkpb.ContentType_AtText: {
+		New: func() any {
+			return &sdkpb.AtTextElem{}
+		},
+		Get: func(oneof any) any {
+			return oneof.(*sdkpb.IMMessage_AtTextElem).AtTextElem
+		},
+		Set: func(msg *sdkpb.IMMessage, elem any) {
+			msg.Content = &sdkpb.IMMessage_AtTextElem{AtTextElem: elem.(*sdkpb.AtTextElem)}
+		},
+	},
+	sdkpb.ContentType_Merge: {
+		New: func() any {
+			return &sdkpb.MergeElem{}
+		},
+		Get: func(oneof any) any {
+			return oneof.(*sdkpb.IMMessage_MergeElem).MergeElem
+		},
+		Set: func(msg *sdkpb.IMMessage, elem any) {
+			msg.Content = &sdkpb.IMMessage_MergeElem{MergeElem: elem.(*sdkpb.MergeElem)}
+		},
+	},
+	sdkpb.ContentType_Card: {
+		New: func() any {
+			return &sdkpb.CardElem{}
+		},
+		Get: func(oneof any) any {
+			return oneof.(*sdkpb.IMMessage_CardElem).CardElem
+		},
+		Set: func(msg *sdkpb.IMMessage, elem any) {
+			msg.Content = &sdkpb.IMMessage_CardElem{CardElem: elem.(*sdkpb.CardElem)}
+		},
+	},
+	sdkpb.ContentType_Location: {
+		New: func() any {
+			return &sdkpb.LocationElem{}
+		},
+		Get: func(oneof any) any {
+			return oneof.(*sdkpb.IMMessage_LocationElem).LocationElem
+		},
+		Set: func(msg *sdkpb.IMMessage, elem any) {
+			msg.Content = &sdkpb.IMMessage_LocationElem{LocationElem: elem.(*sdkpb.LocationElem)}
+		},
+	},
+	sdkpb.ContentType_Custom: {
+		New: func() any {
+			return &sdkpb.CustomElem{}
+		},
+		Get: func(oneof any) any {
+			return oneof.(*sdkpb.IMMessage_CustomElem).CustomElem
+		},
+		Set: func(msg *sdkpb.IMMessage, elem any) {
+			msg.Content = &sdkpb.IMMessage_CustomElem{CustomElem: elem.(*sdkpb.CustomElem)}
+		},
+	},
+	sdkpb.ContentType_Typing: {
+		New: func() any {
+			return &sdkpb.TypingElem{}
+		},
+		Get: func(oneof any) any {
+			return oneof.(*sdkpb.IMMessage_TypingElem).TypingElem
+		},
+		Set: func(msg *sdkpb.IMMessage, elem any) {
+			msg.Content = &sdkpb.IMMessage_TypingElem{TypingElem: elem.(*sdkpb.TypingElem)}
+		},
+	},
+	sdkpb.ContentType_Quote: {
+		New: func() any {
+			return &sdkpb.QuoteElem{}
+		},
+		Get: func(oneof any) any {
+			return oneof.(*sdkpb.IMMessage_QuoteElem).QuoteElem
+		},
+		Set: func(msg *sdkpb.IMMessage, elem any) {
+			msg.Content = &sdkpb.IMMessage_QuoteElem{QuoteElem: elem.(*sdkpb.QuoteElem)}
+		},
+	},
+	sdkpb.ContentType_Face: {
+		New: func() any {
+			return &sdkpb.FaceElem{}
+		},
+		Get: func(oneof any) any {
+			return oneof.(*sdkpb.IMMessage_FaceElem).FaceElem
+		},
+		Set: func(msg *sdkpb.IMMessage, elem any) {
+			msg.Content = &sdkpb.IMMessage_FaceElem{FaceElem: elem.(*sdkpb.FaceElem)}
+		},
+	},
+	sdkpb.ContentType_AdvancedText: {
+		New: func() any {
+			return &sdkpb.AdvancedTextElem{}
+		},
+		Get: func(oneof any) any {
+			return oneof.(*sdkpb.IMMessage_AdvancedTextElem).AdvancedTextElem
+		},
+		Set: func(msg *sdkpb.IMMessage, elem any) {
+			msg.Content = &sdkpb.IMMessage_AdvancedTextElem{AdvancedTextElem: elem.(*sdkpb.AdvancedTextElem)}
 		},
 	},
 }
@@ -84,9 +228,27 @@ func stringToMsgContent(ctx context.Context, content string, msg *sdkpb.IMMessag
 		return errs.ErrArgs.WrapMsg("not support content type")
 	}
 	elem := m.New()
-	if err := utils.JsonStringToStruct(content, elem); err != nil {
-		log.ZError(ctx, "mustStringToMsgContent failed", err, "msg", msg, "content", content)
+	var err error
+	if m.Notification {
+		err = unmarshalNotificationElem(content, elem)
+	} else {
+		err = utils.JsonStringToStruct(content, elem)
+	}
+	if err != nil {
+		log.ZError(ctx, "stringToMsgContent failed", err, "notification", m.Notification, "msg", msg, "content", content)
 	}
 	m.Set(msg, elem)
 	return nil
+}
+
+func unmarshalNotificationElem(content string, tips any) error {
+	var elem notificationElem
+	if err := utils.JsonStringToStruct(content, &elem); err != nil {
+		return err
+	}
+	return utils.JsonStringToStruct(elem.Detail, tips)
+}
+
+type notificationElem struct {
+	Detail string `json:"detail,omitempty"`
 }
