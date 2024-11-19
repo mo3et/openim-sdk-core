435ae3ed0d0b5413bcc7530901339967802d135c
withchao <993506633@qq.com>
2024-11-19 14:20:20 +0800

    format pb

--- internal/user/server_api.go
+++ internal/user/server_api.go
@@ -2,6 +2,7 @@ package user
 
 import (
 	"context"
+	"github.com/openimsdk/tools/errs"
 
 	"github.com/openimsdk/openim-sdk-core/v3/pkg/api"
 	"github.com/openimsdk/protocol/sdkws"
@@ -11,6 +12,17 @@ import (
 func (u *User) getUsersInfo(ctx context.Context, userIDs []string) ([]*sdkws.UserInfo, error) {
 	req := &user.GetDesignateUsersReq{UserIDs: userIDs}
 	return api.ExtractField(ctx, api.GetUsersInfo.Invoke, req, (*user.GetDesignateUsersResp).GetUsersInfo)
+}
+
+func (u *User) getUserInfo(ctx context.Context, userID string) (*sdkws.UserInfo, error) {
+	users, err := u.getUsersInfo(ctx, []string{userID})
+	if err != nil {
+		return nil, err
+	}
+	if len(users) == 0 {
+		return nil, errs.ErrRecordNotFound.WrapMsg("user id not found")
+	}
+	return users[0], nil
 }
 
 func (u *User) updateUserInfo(ctx context.Context, userInfo *sdkws.UserInfoWithEx) error {
