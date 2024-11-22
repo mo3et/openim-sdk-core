fed18a3645c19edd0bc952276cfde52f69e87d29
Gordon <46924906+FGadvancer@users.noreply.github.com>
2024-11-22 10:47:00 +0800

    refactor: the SDK interface using the pb protocol to replace json.
    Signed-off-by: Gordon <46924906+FGadvancer@users.noreply.github.com>

--- sdk_struct/sdk_struct.go
+++ sdk_struct/sdk_struct.go
@@ -332,16 +332,3 @@ type CmdMsgSyncInReinstall struct {
 	Msgs  map[string]*sdkws.PullMsgs
 	Total int
 }
-
-type BasicInfo struct {
-	Nickname string
-	FaceURL  string
-}
-
-type PublicUser struct {
-	UserID     string `json:"userID"`
-	Nickname   string `json:"nickname"`
-	FaceURL    string `json:"faceURL"`
-	Ex         string `json:"ex"`
-	CreateTime int64  `json:"createTime"`
-}
