6ab16f29542b53f905f1de976b06195af104ce8e
Gordon <46924906+FGadvancer@users.noreply.github.com>
2024-11-25 18:09:26 +0800

    refactor: the SDK interface using the pb protocol to replace json.
    Signed-off-by: Gordon <46924906+FGadvancer@users.noreply.github.com>

--- test/config.go
+++ test/config.go
@@ -2,19 +2,20 @@ package test
 
 import "github.com/openimsdk/protocol/constant"
 
-//const (
-//	APIADDR = "http://43.134.224.79:10002"
-//	WSADDR  = "ws://43.134.224.79:10001"
-//
-//	UserID = "6196196276"
-//)
-
 const (
-	APIADDR = "http://172.16.8.135:10002"
-	WSADDR  = "ws://172.16.8.135:10001"
+	APIADDR = "http://43.134.224.79:10002"
+	WSADDR  = "ws://43.134.224.79:10001"
 
-	UserID = "4703677104"
+	UserID = "3325086438"
 )
+
+//
+//const (
+//	APIADDR = "http://172.16.8.135:10002"
+//	WSADDR  = "ws://172.16.8.135:10001"
+//
+//	UserID = "4703677104"
+//)
 
 const (
 	PlatformID  = constant.LinuxPlatformID
