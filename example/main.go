c548b4eb45c2fe72fae968632a62d012f2ca8c32
yejian <596306006@qq.com>
2024-12-07 18:19:38 +0800

    feat: add example flag args

--- example/main.go
+++ example/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"flag"
 	"github.com/AllenDang/cimgui-go/backend"
 	ebitenbackend "github.com/AllenDang/cimgui-go/backend/ebiten-backend"
 	"github.com/AllenDang/cimgui-go/imgui"
@@ -21,9 +22,16 @@ func loop() {
 }
 
 func main() {
-	userId := "6521273309"
-	token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySUQiOiI2NTIxMjczMzA5IiwiUGxhdGZvcm1JRCI6MywiZXhwIjoxNzQxMzIzMjI5LCJuYmYiOjE3MzM1NDY5MjksImlhdCI6MTczMzU0NzIyOX0.K4Mx8b2brgPP2El_mfjSI9b6ZZKADEtjWTpNMjyCJXk"
-	nickName := ui.Login(userId, token)
+	userId := flag.String("userId", "", "User ID ")
+	token := flag.String("token", "", "User Token")
+	flag.Parse()
+
+	if *userId == "" || *token == "" {
+		flag.Usage()
+		return
+	}
+
+	nickName := ui.Login(*userId, *token)
 
 	currentBackend, _ = backend.CreateBackend(ebitenbackend.NewEbitenBackend())
 	currentBackend.SetBgColor(imgui.NewVec4(0.45, 0.55, 0.6, 1.0))
