278efdff6b66639afad565ddf9c1e9e1030f4f1e
Gordon <46924906+FGadvancer@users.noreply.github.com>
2024-12-09 19:36:12 +0800

    refactor: the SDK interface using the pb protocol to replace json.
    Signed-off-by: Gordon <46924906+FGadvancer@users.noreply.github.com>

--- c_cpp_wrapper/test.go
+++ c_cpp_wrapper/test.go
@@ -1,8 +1,8 @@
 package main
 
 /*
-#cgo LDFLAGS: -L. -lyour_library
-#include "your_library.h"
+#cgo LDFLAGS: -L. -llibopenimsdk
+#include "libopenimsdk.h"
 #include <stdlib.h>
 #include <stdint.h>
 
@@ -12,18 +12,22 @@ extern void callback(void* dataPtr, int len);
 import "C"
 
 import (
+	"encoding/json"
 	"fmt"
-	commonpb "github.com/openimsdk/openim-sdk-core/v3/proto/go/common"
+	"sync/atomic"
 	"time"
 	"unsafe"
 
 	"google.golang.org/protobuf/proto"
 
+	commonpb "github.com/openimsdk/openim-sdk-core/v3/proto/go/common"
 	converationpb "github.com/openimsdk/openim-sdk-core/v3/proto/go/conversation"
 	eventpb "github.com/openimsdk/openim-sdk-core/v3/proto/go/event"
 	ffipb "github.com/openimsdk/openim-sdk-core/v3/proto/go/ffi"
 	initpb "github.com/openimsdk/openim-sdk-core/v3/proto/go/init"
 )
+
+var handleCounter atomic.Uint64
 
 //export callback
 func callback(dataPtr unsafe.Pointer, len C.int) {
@@ -77,17 +81,20 @@ func callback(dataPtr unsafe.Pointer, len C.int) {
 	// Print the unmarshaled response
 	fmt.Printf("Received response: %s\n", response.String())
 }
+func GenerateHandleID() uint64 {
+	return handleCounter.Add(1)
+}
 
 func main() {
 	// Set the callback and call ffi_init_c
 	callback := C.CallBack(C.callback)
-	handleID := C.ffi_init(callback)
+	handleID := C.ffi_init(callback, 1)
 	fmt.Printf("Initialized handle ID: %d\n", handleID)
 
 	// Marshal the request data
-	data, err := proto.Marshal(&initpb.InitSDKReq{
+	data, err := json.Marshal(&initpb.InitSDKReq{
 		Config: &initpb.IMConfig{
-			AppFramework:        commonpb.AppFramework(commonpb.AppFramework_value["flutter"]),
+			AppFramework:        commonpb.AppFramework_Flutter,
 			Platform:            2,
 			ApiAddr:             "https://web.rentsoft.cn/api",
 			WsAddr:              "wss://web.rentsoft.cn/msg_gateway",
@@ -106,8 +113,9 @@ func main() {
 	req := ffipb.FfiRequest{
 		FuncName: eventpb.FuncRequestEventName_InitSDK,
 		Data:     data,
+		HandleID: GenerateHandleID(),
 	}
-	ffiReq, err := proto.Marshal(&req)
+	ffiReq, err := json.Marshal(&req)
 	if err != nil {
 		fmt.Println("FfiRequest proto.Marshal failed:", err)
 		return
@@ -115,50 +123,34 @@ func main() {
 
 	// Call ffi_request with sample data
 	length := C.int(len(ffiReq))
-	ret := C.ffi_request(unsafe.Pointer(&ffiReq[0]), length)
-	fmt.Printf("ffi_request returned: %d\n", ret)
-
-	//data, err = proto.Marshal(&pb.SetConnListenerReq{})
-	req = ffipb.FfiRequest{
-		//FuncName: eventpb.FuncRequestEventName_SetConnListener,
-		Data: data,
-	}
-	ffiReq, err = proto.Marshal(&req)
-	if err != nil {
-		fmt.Println("FfiRequest proto.Marshal failed:", err)
-		return
-	}
-
-	length = C.int(len(ffiReq))
-	ret = C.ffi_request(unsafe.Pointer(&ffiReq[0]), length)
-	fmt.Printf("ffi_request returned: %d\n", ret)
+	C.ffi_request(unsafe.Pointer(&ffiReq[0]), length)
 
 	time.Sleep(time.Second * 1)
 
-	data, err = proto.Marshal(&initpb.LoginReq{
-		UserID: "3325086438",
-		Token:  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySUQiOiIzMzI1MDg2NDM4IiwiUGxhdGZvcm1JRCI6MiwiZXhwIjoxNzM3NDQ5Nzg2LCJuYmYiOjE3Mjk2NzM0ODYsImlhdCI6MTcyOTY3Mzc4Nn0.obyGZDP3yFiTT7tYkkdEkhAjDtSJI-z9-17FUXXJcxY",
-	})
-	if err != nil {
-		fmt.Println("GetAllConversationListReq proto.Marshal failed:", err)
-		return
-	}
-
-	// Create and marshal the FfiRequest
-	req = ffipb.FfiRequest{
-		FuncName: eventpb.FuncRequestEventName_Login,
-		Data:     data,
-	}
-	ffiReq, err = proto.Marshal(&req)
-	if err != nil {
-		fmt.Println("FfiRequest proto.Marshal failed:", err)
-		return
-	}
-
-	// Call ffi_request with sample data
-	length = C.int(len(ffiReq))
-	ret = C.ffi_request(unsafe.Pointer(&ffiReq[0]), length)
-	fmt.Printf("ffi_request returned: %d\n", ret)
+	//data, err = proto.Marshal(&initpb.LoginReq{
+	//	UserID: "3325086438",
+	//	Token:  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySUQiOiIzMzI1MDg2NDM4IiwiUGxhdGZvcm1JRCI6MiwiZXhwIjoxNzM3NDQ5Nzg2LCJuYmYiOjE3Mjk2NzM0ODYsImlhdCI6MTcyOTY3Mzc4Nn0.obyGZDP3yFiTT7tYkkdEkhAjDtSJI-z9-17FUXXJcxY",
+	//})
+	//if err != nil {
+	//	fmt.Println("GetAllConversationListReq proto.Marshal failed:", err)
+	//	return
+	//}
+	//
+	//// Create and marshal the FfiRequest
+	//req = ffipb.FfiRequest{
+	//	FuncName: eventpb.FuncRequestEventName_Login,
+	//	Data:     data,
+	//	HandleID: GenerateHandleID(),
+	//}
+	//ffiReq, err = proto.Marshal(&req)
+	//if err != nil {
+	//	fmt.Println("FfiRequest proto.Marshal failed:", err)
+	//	return
+	//}
+	//
+	//// Call ffi_request with sample data
+	//length = C.int(len(ffiReq))
+	//C.ffi_request(unsafe.Pointer(&ffiReq[0]), length)
 
 	//
 	//// Release the handle after use
