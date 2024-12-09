a4db9eea6c6ed81653651ae8417cbed7b60f7090
Gordon <46924906+FGadvancer@users.noreply.github.com>
2024-12-09 20:30:49 +0800

    feat: add ffi interface for go mobile.
    Signed-off-by: Gordon <46924906+FGadvancer@users.noreply.github.com>

---
+++ bindings/gomobile/ffi_handler.go
@@ -0,0 +1,30 @@
+package gomobile
+
+import "C"
+import (
+	"github.com/openimsdk/openim-sdk-core/v3/bindings/base"
+)
+
+var currentMobileCallback mobileCallback
+
+type mobileCallback interface {
+	onData([]byte)
+}
+
+func init() {
+	base.SetDispatchFfiResultFunc(dispatchResultForGoMobile)
+}
+
+func dispatchResultForGoMobile(_ uint64, data []byte) {
+	currentMobileCallback.onData(data)
+}
+
+func FfiInit(cb mobileCallback, protocolType int) uint64 {
+	base.SetProtocolType(protocolType)
+	currentMobileCallback = cb
+	return 1
+}
+
+func FfiRequest(mobileData []byte) {
+	base.FfiRequest(mobileData)
+}
