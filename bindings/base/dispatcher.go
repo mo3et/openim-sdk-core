736a83d5cb7bf98770fe83e496b3d10e345aae85
Gordon <46924906+FGadvancer@users.noreply.github.com>
2024-12-14 11:34:46 +0800

    fix: add server isEnd determination criteria for message retrieval.
    Signed-off-by: Gordon <46924906+FGadvancer@users.noreply.github.com>

--- bindings/base/dispatcher.go
+++ bindings/base/dispatcher.go
@@ -53,6 +53,7 @@ func GoFfiRequestHandler(ctx context.Context, funcName pb.FuncRequestEventName, 
 		FuncName:    funcName,
 		Data:        data,
 	}
+	log.ZDebug(ctx, "GoFfiRequestHandler", "ffiRequest", ffiRequest.String())
 	ffiRequestData, err := serializer.GetInstance().Marshal(ffiRequest)
 	if err != nil {
 		return nil, sdkerrs.ErrArgs.Wrap()
--- bindings/wasm/handlers/ffi_handler.go
+++ bindings/wasm/handlers/ffi_handler.go
@@ -44,6 +44,7 @@ func sendRequestToJs(ctx context.Context, _ uint64, data []byte) ([]byte, error)
 		return nil
 	})
 	defer catchFunc.Release()
+	log.ZWarn(ctx, "sendRequestToJs", "data", data)
 	reqCallBack.Invoke(JSUint8ArrayFromGoBytes(data)).Call("then", thenFunc).Call("catch", catchFunc)
 	select {
 	case <-ctx.Done():
